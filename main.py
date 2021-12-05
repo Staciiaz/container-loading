fc0767d17205220719011b55ecc1aea8822e0c90
SeaAc132 <chollakorn.nimp@gmail.com>
2021-12-06 02:06:38 +0700

    final ?

--- main.py
+++ main.py
@@ -18,20 +18,15 @@ def read_input(file_name):
 def main():
     Box.initialize()
     Stacking.initialize()
-    Container.stackable_types = {
-        '101': [['HU', 'HU'], ['U', 'U']]
-    }
     input_boxes = read_input('data/input_boxes.txt')
-    process(input_boxes)
-    # processor = Processor()
-    # output_stacking = processor.process(input_boxes)
-    # print('Input Boxes:', input_boxes)
-    # # print('Stacking:', output_stacking)
-    # # print('Remaining boxes:', processor.remaining_boxes)
-    # print('Output Stacking:')
-    # for stacking_type, stacking_amount in sorted(output_stacking.items(), key=lambda x: Stacking.get(x[0]).size[0]):
-    #     stacking = Stacking.get(stacking_type)
-    #     print(stacking.type_id, stacking.size, stacking_amount)
+    processor = Processor()
+    output_stacking = processor.process(input_boxes)
+    print('Input Boxes:', input_boxes)
+    print('Remaining boxes:', processor.remaining_boxes)
+    print('Output Stacking:')
+    for stacking_type, stacking_amount in sorted(output_stacking.items(), key=lambda x: Stacking.get(x[0]).size[0]):
+        stacking = Stacking.get(stacking_type)
+        print(stacking.type_id, stacking.size, stacking_amount)
 
 
 if __name__ == '__main__':
